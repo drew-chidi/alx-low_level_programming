6433b61ccb7293a599cd799cb8c4cb122798dd9c
root <chidi.anAndrew Ofuenweuche>
2022-04-08 16:39:49 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int lw = 'a';
+	while (lw <= 'z')
+	{
+		if (lw == 'e' || lw == 'q')
+		{
+			lw += 1;
+		}
+		else
+		{
+			putchar(lw);
+			lw += 1;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
