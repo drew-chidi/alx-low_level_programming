f0bd3f2c6252e6cb55411be0d990f17887cacb48
root <chidi.anAndrew Ofuenweuche>
2022-04-08 16:59:19 -0700

    print comb.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - program that prints the alphabet in lowercase, followed by a new line
+ * You can only use the putchar
+ * Return: 0
+ */
+
 int main(void)
 {
 	int lw = 'a';
+
 	while (lw <= 'z')
 	{
 		if (lw == 'e' || lw == 'q')
