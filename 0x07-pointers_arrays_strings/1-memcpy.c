bcdce2096aa5dadf4f860f49a8db8833a9517599
root <chidi.anAndrew Ofuenweuche>
2022-04-27 17:22:59 -0700

    HOT FIX

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,11 +13,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 	
-	i = 0;
-	while (i < n)  /*Delacring WHILE*/
+	for (i = 0; i < n; i++)
 	{
-		*(dest + i) = *(src + i); /*add 1 position dest and src*/
-		i++;
-	} /*END WHILE*/
+		*(dest + i) = *(src + i);
+	}
 	return (dest);
 }
