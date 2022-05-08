2bfc2fb6e53980b746a6a6525b67c263faad161b
root <chidi.anAndrew Ofuenweuche>
2022-05-07 18:23:06 -0700

    hot fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -43,7 +43,7 @@ int sum = 0;
 count = 1;
 while (count < argc) /*Goes through the whole array*/
 {
-if(check_num(argv[count]))
+if (check_num(argv[count]))
 {
 str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 sum += str_to_int;
