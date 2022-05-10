9c4ab555c74b97b5bfc35909d8cd729f4cb71c56
root <chidi.anAndrew Ofuenweuche>
2022-05-09 17:48:22 -0700

    hot fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,6 +6,7 @@
  *@size: bytes for each position in array
  *Return: pointer void
  */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *p;
@@ -20,6 +21,5 @@ return (NULL);
 
 for (i = 0; i < nmemb * size; i++)
 p[i] = 0;
-
 return (p);
 }
